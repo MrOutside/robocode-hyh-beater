cd47eb97a81e2fb3215b1a77387c14303e67fc43
yonghonghsu@gmail.com <yonghonghsu@gmail.com@04d50573-650d-2122-01e1-e7e7b652f8bc>
2011-11-09 00:53:18 +0000

    modified robocode

--- trunk/src/hyh/EnemyBot.java
+++ trunk/src/hyh/EnemyBot.java
@@ -2,87 +2,95 @@ package hyh;
 
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
+
 /**
  * This tests for variability in bullet power administered by the robot DaCruzer.
- *
+ * 
  * @author Yong Hong Hsu.
  */
 public class EnemyBot extends Robot {
-  //bearing, distance, energy, heading, name, velocity. 
-  //All of these will be of type double except for name which will be of type String. 
+  // bearing, distance, energy, heading, name, velocity.
+  // All of these will be of type double except for name which will be of type String.
   private double bearing = 0;
   private double distance = 0;
   private double energy = 0;
   private double heading = 0;
   private String name = null;
   private double velocity = 0;
-  //Lastly, implement a public constructor which just calls reset. Note: the constructor
-  //must be the same name as the class. Also, constructors never specify a return value. 
+
+  // Lastly, implement a public constructor which just calls reset. Note: the constructor
+  // must be the same name as the class. Also, constructors never specify a return value.
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * 
    */
-  public EnemyBot() {
-      reset();
-  }
-  //getBearing(), getDistance(), getEnergy(), getHeading(), getName(), getVelocity(). 
+  // public EnemyBot() {
+  // reset();
+  // }
+  // getBearing(), getDistance(), getEnergy(), getHeading(), getName(), getVelocity().
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * @return bearing bearing.
    */
   public double getBearing() {
     return bearing;
   }
+
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * @return distance distance.
    */
   public double getDistance() {
-      return distance;
+    return distance;
   }
+
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * @return energy energy.
    */
   public double getEnergy() {
-      return energy;
+    return energy;
   }
+
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * @return heading heading
    */
   public double getHeading() {
-      return heading;
+    return heading;
   }
+
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * @return name name
    */
   public String getName() {
-      return name;
+    return name;
   }
+
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * @return velocity velocity.
    */
   public double getVelocity() {
-      return velocity;
+    return velocity;
   }
-  //Implement a state-change method called update which takes a ScannedRobotEvent as a parameter.
-  //Call the ScannedRobotEvent's methods (same names as the ones in step #3). 
-  //to set your private variables (step #2). 
-  //The update method will return void.
+
+  // Implement a state-change method called update which takes a ScannedRobotEvent as a parameter.
+  // Call the ScannedRobotEvent's methods (same names as the ones in step #3).
+  // to set your private variables (step #2).
+  // The update method will return void.
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
-   *@param e event.
+   * 
+   * @param e event.
    */
   public void update(ScannedRobotEvent e) {
     bearing = e.getBearing();
@@ -92,12 +100,13 @@ public class EnemyBot extends Robot {
     name = e.getName();
     velocity = e.getVelocity();
   }
-  //Implement another state-change method called reset which sets.
-  //the name variable to the empty string ("") and. 
-  //all the variables of type double to 0.0. The reset method will also return void.
+
+  // Implement another state-change method called reset which sets.
+  // the name variable to the empty string ("") and.
+  // all the variables of type double to 0.0. The reset method will also return void.
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * 
    */
   public void reset() {
@@ -108,14 +117,15 @@ public class EnemyBot extends Robot {
     name = "";
     velocity = 0;
   }
-  //Implement a (state-reporting) accessor method called none. 
-  //which will return true if name is "" or false otherwise. 
-  //(Remember to use the equals() method of the String class.). 
-  //Basically, this method will return true if the reset method. 
-  //was just called. 
+
+  // Implement a (state-reporting) accessor method called none.
+  // which will return true if name is "" or false otherwise.
+  // (Remember to use the equals() method of the String class.).
+  // Basically, this method will return true if the reset method.
+  // was just called.
   /**
    * This tests for variability in bullet power administered by the robot DaCruzer.
-   *
+   * 
    * @return true when true.
    */
   public boolean none() {
